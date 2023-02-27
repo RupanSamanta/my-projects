8d823c427a2adf82d1b969ef28f01f9eb40a9768
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2023-02-27 16:23:53 +0530

    Add files via upload

---
+++ DLS 23 Squad Builder/data/players.js
@@ -0,0 +1,82 @@
+const DATA = [
+   {
+      name: "Lionel Messi",
+      short: "Messi",
+      url: "Lionel Messi",
+      type: "Legendary",
+      rate: 86, position: "CF",
+      nation: {
+         name: "Argentina",
+         url: ""
+      },
+      height: 170, foot: "Left",
+      stats: [
+         80, 87, 77, 94,
+         69, 55, 91, 90
+      ]
+   },
+   {
+      name: "Erling Haaland",
+      short: "Haaland",
+      url: "Erling Haaland",
+      type: "Legendary",
+      rate: 86, position: "CF",
+      nation: {
+         name: "Norway",
+         url: ""
+      },
+      height: 194, foot: "Left",
+      stats: [
+         90, 83, 79, 81,
+         90, 50, 75, 93
+      ]
+   },
+   {
+      name: "Kylian Mbappé",
+      short: "Mbappé",
+      url: "Kylian Mbappé",
+      type: "Legendary",
+      rate: 86, position: "CF",
+      nation: {
+         name: "France",
+         url: ""
+      },
+      height: 178, foot: "Right",
+      stats: [
+         94, 94, 82, 88,
+         75, 33, 83, 91
+      ]
+   },
+   {
+      name: "Robert Lewandowski",
+      short: "Lewandowski",
+      url: "Eden Hazard",
+      type: "Rare",
+      rate: 85, position: "CF",
+      nation: {
+         name: "Poland",
+         url: ""
+      },
+      height: 185, foot: "Right",
+      stats: [
+         76, 76, 84, 86,
+         88, 51, 80, 94
+      ]
+   },
+   {
+      name: "Cristiano Ronaldo",
+      short: "Ronaldo",
+      url: "Eden Hazard",
+      type: "Legendary",
+      rate: 98, position: "CF",
+      nation: {
+         name: "Poland",
+         url: ""
+      },
+      height: 185, foot: "Right",
+      stats: [
+         76, 76, 84, 86,
+         88, 51, 80, 94
+      ]
+   }
+]
